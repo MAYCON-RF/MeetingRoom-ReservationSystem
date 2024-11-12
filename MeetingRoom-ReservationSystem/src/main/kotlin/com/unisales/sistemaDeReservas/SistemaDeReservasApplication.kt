e07e582f9306fd2ec59be686e18a9656e09a6f83
Maycon Rodrigues <maycon.rf@outlook.com>
2024-11-11 22:39:54 -0300

    subindo projeto para github

---
+++ MeetingRoom-ReservationSystem/src/main/kotlin/com/unisales/sistemaDeReservas/SistemaDeReservasApplication.kt
@@ -0,0 +1,11 @@
+package com.unisales.sistemaDeReservas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaDeReservasApplication
+
+fun main(args: Array<String>) {
+	runApplication<SistemaDeReservasApplication>(*args)
+}
---
+++ MeetingRoom-ReservationSystem/src/test/kotlin/com/unisales/sistemaDeReservas/SistemaDeReservasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unisales.sistemaDeReservas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaDeReservasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
