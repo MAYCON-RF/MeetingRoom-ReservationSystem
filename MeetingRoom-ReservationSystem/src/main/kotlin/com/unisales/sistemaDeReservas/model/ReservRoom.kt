d83c58c8da9af0ce8c1c90a02973b5c5324eade3
Maycon Rodrigues <maycon.rf@outlook.com>
2024-11-13 23:24:19 -0300

    model criada

---
+++ MeetingRoom-ReservationSystem/src/main/kotlin/com/unisales/sistemaDeReservas/model/ReservRoom.kt
@@ -0,0 +1,19 @@
+package com.unisales.sistemaDeReservas.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+class ReservRoom {
+
+    /*Representa uma reserva de sala*/
+    @Document(collection = "reservas")
+    data class ReservRoom(
+        @Id
+        val idReserv: String? = null, /* ID único da reserva, pode ser nulo ao criar porque usa o "?" */
+        val salaId: String,    /* ID da sala que está sendo reservada*/
+        val usuario: String,   /* Nome do usuário que fez a reserva*/
+        val inicio: LocalDateTime, /* Data e hora de início da reserva*/
+        val fim: LocalDateTime   /* Data e hora de término da reserva*/
+    )
+}
---
+++ MeetingRoom-ReservationSystem/src/main/kotlin/com/unisales/sistemaDeReservas/model/Room.kt
@@ -0,0 +1,21 @@
+package com.unisales.sistemaDeReservas.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+/* Representa uma sala de reunião */
+@Document(collection = "salas") /* a anotação @Entity para indicar que essa classe representa uma entidade do JPA.*/
+data class Room(
+    @Id
+    val idRoom: String? = null,            /* ID único da salq pode ser nulo ao criar porque usa o "?" */
+    val nome: String,                      /* Nome da sala*/
+    val capacidade: Int,                   /* Quantidade de pessoas que a sala suporta*/
+    val recursos: List<String>,            /* Lista de recursos (ex: projetor, quadro)*/
+    val status: StatusDaSala               /* Status da sala: ATIVA ou INATIVA*/
+)
+
+// Enum que define os possíveis status das salas
+enum class StatusDaSala {
+    ATIVA,     /* Sala disponível para reserva */
+    INATIVA    /* Sala fora de operação */
+}
